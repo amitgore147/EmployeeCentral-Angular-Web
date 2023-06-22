79e7be78526739c80ffe3b5ae833c260ca66136a
Amit Gore <amitgore147@gmail.com>
2023-06-22 15:17:56 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
+import { AddEmployeeComponent } from './components/employees/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/employees/edit-employee/edit-employee.component';
+import { HomeComponent } from './components/employees/home/home.component';
+import { AboutUsComponent } from './components/employees/about-us/about-us.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'employees',
+    component: EmployeesListComponent
+  },
+  {
+    path: 'employees/add',
+    component: AddEmployeeComponent
+  },
+  {
+    path: 'employees/edit/:id',
+    component: EditEmployeeComponent
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
